ea7e9879c13f76583fd7252789e3cd3f33098587
yeamao <henry3556108@gmail.com>
2018-11-14 01:04:29 +0800

    資料分類並新增readme

---
+++ generation-2/tf_test-2.py
@@ -0,0 +1,57 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+from numpy.random import RandomState
+import numpy as np
+import tensorflow as tf
+
+
+batch_size=8
+#每次訓練的資料量
+w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+x = tf.placeholder(tf.float32,shape = (None,2),name = 'x_input')
+y_ = tf.placeholder(tf.float32,shape = (None),name = 'y_input')
+
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+cross_entropy = -(tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10,1.0))))
+# 交叉熵
+mse = tf.reduce_mean(tf.square(y_ - y))
+#最小均方差
+
+global_step = tf.Variable(3.5)
+
+learning_rate = tf.train.exponential_decay(0.1,global_step,100,0.96,staircase=True)
+#學習率指數型下降 staircase是判斷是否為階梯式下降或者是曲線
+trainstep = tf.train.AdadeltaOptimizer(learning_rate).minimize(cross_entropy,global_step=global_step)
+#優化模型
+rdm = RandomState(1)
+#隨積的seed
+dataset_size=128
+#data的大小
+X = rdm.rand(dataset_size , 2)
+#生成我們的輸入資料X
+Y = [[int(x1+x2 < 1) for (x1 , x2) in X]]
+#生成我們的標準資料Y
+dic={}
+with tf.Session() as sess:
+#宣告了一個session
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    #初始化我們的variables
+    print(sess.run(w1),'\n',sess.run(w2))
+    STEP = 5000
+    #訓練的次數
+    for i in range(STEP):
+        start = (i*batch_size) % dataset_size
+        end = min(start+batch_size,dataset_size)
+        #每次選取不一樣的8筆資料
+        sess.run(trainstep,feed_dict={x:X[start:end] , y_:Y[start:end]})
+        #訓練我們的model 優化權重
+        if i %1000 == 0:
+            cross=sess.run(cross_entropy,feed_dict={x:X,y_:Y})
+            print(i,'\n',cross)
+            
+    print(sess.run(w1),'\n',sess.run(w2))
